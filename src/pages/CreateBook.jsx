d9491666878ff80e6cfcfb5a12bbf5dbea77c90f
Chienpm <chienmp.cn@gmail.com>
2023-11-16 11:18:38 +0700

    validate and delete item

--- src/pages/CreateBook.jsx
+++ src/pages/CreateBook.jsx
@@ -1,9 +1,10 @@
 import BackButton from "../components/BackButton";
-import { useRef, useState } from "react";
+import { useState } from "react";
 import axios from "axios";
 import Spinner from "../components/Spinner";
 import { useNavigate } from "react-router-dom";
-import { Input } from "../components/Input";
+import { useForm } from "react-hook-form";
+
 
 export const CreateBook = () => {
   const [title, setTitle] = useState('');
@@ -11,7 +12,7 @@ export const CreateBook = () => {
   const [date, setDate] = useState('');
   const [loading, setLoading] = useState(false);
   const navigate = useNavigate();
-  const tittleRef =  useRef()
+  const { register, handleSubmit, formState: { errors } } = useForm();
   const handleCreateBook = () => {
     const data = {
       title,
@@ -37,10 +38,12 @@ export const CreateBook = () => {
       { loading ? (
         <Spinner></Spinner>
       ) : (
+        <form>
         <div className="flex flex-col border-2 border-sky-700 rounded-xl w-[600px] p-4 mx-auto">
           <div className="my-4">
             <label className="text-xl mr-4 text-gray-500">Title</label>
-            <Input setTitle={setTitle} refer={tittleRef} />
+            <input type="text" {...register('title', {required : true})} value={title} onChange={(e) => setTitle(e.target.value)} className="border-2 border-gray-300 px-4 py-2 w-full"  aria-invalid={errors.firstName ? "true" : "false"} ></input>
+            {errors.title?.type === 'required' && <p role="alert">Title is required</p>}
           </div>
           <div className="my-4">
             <label className="text-xl mr-4 text-gray-500">Author</label>
@@ -50,8 +53,9 @@ export const CreateBook = () => {
             <label className="text-xl mr-4 text-gray-500">Title</label>
             <input type="date" value={date} onChange={(e) => setDate(e.target.value)} className="border-2 border-gray-300 px-4 py-2 w-full"></input>
           </div>
-          <button onClick={handleCreateBook} className="p-2 m-8 bg-blue-100">Save</button>
+          <button onClick={handleSubmit(handleCreateBook)} className="p-2 m-8 bg-blue-100">Save</button>
         </div>
+        </form>
       )}
     </div>
   )
--- src/pages/DeleteBook.jsx
+++ src/pages/DeleteBook.jsx
@@ -1,8 +1,36 @@
-import React from 'react'
+import BackButton from "../components/BackButton";
+import { useState } from "react";
+import axios from "axios";
+import Spinner from "../components/Spinner";
+import { useNavigate, useParams } from "react-router-dom";
 
 export const DeleteBook = () => {
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+  const {id} = useParams();
+  const handleDeleteBook = () => {
+    setLoading(true);
+    axios
+      .delete("http://localhost:5555/books/" + id)
+      .then(() => {
+        navigate("/");
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.log(error);
+        setLoading(false);
+      });
+  };
   return (
-    <div>DeleteBook</div>
+    <div className="p-4">
+      <BackButton></BackButton>
+      <h1 className="text-3xl">Delete Book</h1>
+      {loading ? <Spinner></Spinner> : ''}
+      <div className="flex flex-col items-center border-2 border-sky-300 rounded-xl w-[600px] p-8 mx-auto">
+        <h3 className="text-2xl">Are you sure want to delete this book ?</h3>
+        <button className="p-4 bg-red-600 text-white m-8 w-full" onClick={handleDeleteBook}>Yes, delete it</button>
+      </div>
+    </div>
   )
 }
 
